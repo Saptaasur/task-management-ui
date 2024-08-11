12612aaba471d74cb90ead1de959db8bce7904c7
Saptarshi Nandy <saptarshiiiiiiiiii@gmail.com>
2024-08-11 20:47:55 +0530

    axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://task-management-backend-mf4f.onrender.com', // Set the base URL to your backend
+  baseURL: 'https://task-management-backend-k2cg.onrender.com', // Set the base URL to your backend
   headers: {
     'Content-Type': 'application/json',
   },
